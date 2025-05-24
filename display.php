9e5f11e10f85034709e1f410ee7e9b6f32ccb788
RJ-lok <jagdishlokhande454@gmail.com>
2025-05-24 09:43:10 +0530

    add

---
+++ display.php
@@ -0,0 +1,27 @@
+
+<?php
+$pass="your_new_password";
+$conn = pg_connect("host=localhost dbname=se05 user=postgres password=$pass");
+if (!$conn) {
+    die("Connection failed: " . pg_last_error());
+}
+
+    $query = "SELECT e_name,plus FROM ele";
+    $result = pg_query($conn, $query);
+
+    if (pg_num_rows($result) > 0) {
+        echo "Live Result is:<br>";
+        echo "<table border='1'>
+                <tr> <th>Candidate name</th> 
+                <th>vote</th> </tr>";
+        while($row = pg_fetch_assoc($result)) 
+        {
+            echo "<tr> 
+                    <td>{$row['e_name']}</td>
+                    <td>{$row['plus']}</td>
+                  </tr>";
+        }
+        echo "</table>";
+    } 
+pg_close($conn);
+?>
