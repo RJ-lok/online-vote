216856a5a54985c300c47134e9aed0f71a964ee6
RJ-lok <jagdishlokhande454@gmail.com>
2025-06-27 23:08:39 +0530

    add

--- README.md
+++
@@ -1,2 +0,0 @@
-# online-vote
-online voting system
--- script.js
+++
@@ -1,8 +0,0 @@
-["num1", "num2"].forEach(id => {
-  document.getElementById(id).oninput = function () {
-    if (/[^0-9]/.test(this.value)) {
-      alert("Only numbers allowed");
-      this.value = this.value.replace(/[^0-9]/g, '');
-    }
-  };
-});
