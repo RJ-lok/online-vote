ef2eecb8228630c0444fbcc785777af5aab3f96b
RJ-lok <jagdishlokhande454@gmail.com>
2025-09-30 22:30:02 +0530

    Your commit message here

--- vote.php
+++ vote.php
@@ -3,7 +3,7 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-$pass="your_new_password";
+$pass = "your_new_password";
 $conn = pg_connect("host=localhost port=5432 dbname=project user=postgres password=$pass");
 
 if (!$conn) {
@@ -11,57 +11,51 @@ if (!$conn) {
 }
 
 function showMessage($message, $showForm = false, $vote_id = '') {
+    global $conn; 
     ?>
     <!DOCTYPE html>
     <html lang="en">
     <head>
         <meta charset="UTF-8" />
         <title>Voting Page</title>
-<link rel="stylesheet" href="vote.css" />
-
+        <link rel="stylesheet" href="vote.css" />
     </head>
     <body>
         <div class="screen-center">
             <div class="box">
                 <h3><?= htmlspecialchars($message) ?><br></h3>
+
 <?php if ($showForm): ?>
     <form method="post" action="submit_vote.php" class="vote-form">
         <input type="hidden" name="vote_id" value="<?= htmlspecialchars($vote_id) ?>">
-             <label class="vote-option">
-            <input type="radio" name="candidate" value="Rutuja" required>
-            <img src="Rutuja.jpeg" alt="Rutuja" class="candidate-img">
-            Rutuja
-        </label>
-        
-       <br>
 
-        <label class="vote-option">
-            <input type="radio" name="candidate" value="Shitija">
-            <img src="Shitija.jpg" alt="Shitija" class="candidate-img">
-            Shitija
-        </label><br>
+        <?php
+        $candidate_query = "SELECT * FROM ele";
+        $result = pg_query($conn, $candidate_query);
 
-        <label class="vote-option">
-            <input type="radio" name="candidate" value="Rajesh">
-            <img src="Rajesh.jpeg" alt="Rajesh" class="candidate-img">
-            Rajesh
-        </label><br>
+        if ($result) {
+            while ($row = pg_fetch_assoc($result)) {
+                echo "
+                <label class='vote-option'>
+                    <input type='radio' name='candidate' value='{$row['e_name']}' required>
+                    <img src='{$row['symbol']}' alt='{$row['e_name']}' class='candidate-img'>
+                    <div><strong>{$row['e_name']}</strong></div>
+                </label><br>
+                ";
+            }
+        } else {
+            echo "<p>No candidates found.</p>";
+        }
+        ?>
 
-        <label class="vote-option">
-            <input type="radio" name="candidate" value="Rina">
-            <img src="Rina.png" alt="Rina" class="candidate-img">
-            Rina
-        </label>
         <div class="button-group">
-        <input type="submit" value="Vote" class="vote-button">
-
+            <input type="submit" value="Vote" class="vote-button">
+        </div>
     </form>
 <?php endif; ?>
 
-
     <a href="index.html" class="back-button">Back to Home</a>     
-       </div>
-</div>
+            </div>
         </div>
     </body>
     </html>
